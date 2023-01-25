5a83987d58bbe7035185d5a1d437c50214f6d371
rickyzatnika <rickyzatnika91@gmail.com>
2023-01-25 21:43:38 +0700

    update about section

--- components/Home/AboutSection.jsx
+++ components/Home/AboutSection.jsx
@@ -9,7 +9,7 @@ const AboutSection = () => {
       <div className="w-full h-full bg-gradient-to-r from-indigo-800/50 relative to-transparent py-20">
         <div className="absolute left-0 -top-10 w-52 h-52 bg-pink-600/20 rounded-full blur-lg -z-50" />
         <div className="absolute right-0 -bottom-20 w-80 h-80 bg-purple-600/30 rounded-full blur-lg -z-50" />
-        <div className="grid grid-cols-1 lg:grid-cols-12 items-center-gap-4">
+        <div className="grid grid-cols-1 lg:grid-cols-12 items-center gap-8 lg:gap-4">
           <motion.div
             initial={{ opacity: 0, y: 90 }}
             whileInView={{ opacity: 1, y: 0 }}
@@ -41,17 +41,28 @@ const AboutSection = () => {
             initial={{ opacity: 0, y: 100 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.9, delay: 0.6 }}
-            className="col-span-1 lg:col-span-6"
+            className="col-span-1 lg:col-span-6 relative flex flex-col lg:block items-center gap-2"
           >
             <div className="w-full h-full">
               <Image
-                src="/images/test.png"
+                src="/images/dashboard-app.png"
                 alt="about-us"
-                width={75}
-                height={50}
+                width={100}
+                height={75}
                 layout="responsive"
-                objectFit="contain"
+                objectFit="cover"
                 objectPosition="center"
+                priority
+              />
+            </div>
+            <div className="relative lg:absolute lg:-bottom-20 lg:right-8  ">
+              <Image
+                src="/images/stats.png"
+                alt=""
+                width={325}
+                height={250}
+                layout="fixed"
+                objectFit="cover"
                 priority
               />
             </div>
